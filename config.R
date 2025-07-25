fd10394339cd69648ed8d28f314d9c64079e6cd4
你的名字 <>
2025-07-26 01:52:17 +0800

    feat: 刪除config

--- config.R
+++
@@ -1,8 +0,0 @@
-host <- Sys.info()[["nodename"]]
-
-data_dir <- switch(host,
-                   "DESKTOP-3E1J9LG" = "F:/淡江/研究實習生/",
-                   "Brian-PC" = "E:/Research/weather/",
-                   "Lab-PC" = "C:/Users/Researcher/Data/",
-                   stop("請加入此電腦的資料路徑到 config.R")
-)
