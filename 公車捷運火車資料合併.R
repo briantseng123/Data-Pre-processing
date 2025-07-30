2c4f4cf5866597e09958a0b5eef314b5a80e3c6c
Brian Tseng <brian.tseng33@gmail.com>
2025-07-31 00:04:57 +0800

    feat: 添加臺北資料的處理過程

--- 公車捷運火車資料合併.R
+++ 公車捷運火車資料合併.R
@@ -42,7 +42,7 @@
   keep_cols <- c("Authority", "HolderType", "TicketType", "SubTicketType",
                  "BoardingTime","DeboardingTime","BoardingStopName","DeboardingStopName",
                  "BoardingStopUID","DeboardingStopUID","Distance","Bdevelopment_level","Ddevelopment_level",
-                 "dev_movement","movement_level")
+                 "dev_movement","movement_level","Bweather_grid_id","Dweather_grid_id","TransferCode")
   hourtrans <- function(df, target_timezone = "Asia/Taipei"){
     tz_diff_hours <- as.numeric(difftime(
       force_tz(as.POSIXct("2000-01-01 00:00:00", tz = "UTC"), tzone = target_timezone),
@@ -69,6 +69,21 @@
       theme_minimal() +
       scale_x_continuous(breaks = seq(0, 23, by = 2)) 
   }
+  read_fst_folder <- function(folder, cols, pattern = "\\.fst$") {
+    files <- list.files(path = folder, pattern = pattern, full.names = TRUE)
+    
+    dt_list <- lapply(files, function(f) {
+      cat("讀取", f, "…\n")
+      df <- read_fst(f, columns = cols)
+      df <- as.data.table(df)
+      
+      return(df)
+    })
+    
+    cat("合併中...\n")
+    combined <- rbindlist(dt_list, use.names = TRUE)
+    return(combined)
+  }
   mrt <- fread("E:/brain/解壓縮data/資料處理/交通站點資料/Kriging格點/北台灣捷運站點(加入鄉政市區數位發展分類與Kriging天氣格點).csv",encoding = "UTF-8")
   
   # 新北捷運
@@ -90,6 +105,27 @@
     write_fst(NTPmrtdf,"E:/brain/解壓縮data/資料處理/對齊後資料/2024新北捷運最終資料.fst")
   }
   
+  # 臺北捷運
+  {
+    TPEmrtdf <- read_fst_folder("E:/brain/解壓縮data/資料處理/2022/2022臺北市捷運1-6月(發展程度移動_kriging_v3)5chunk","BoardingTime")
+    TPEmrtdf <- read_fst_folder("E:/brain/解壓縮data/資料處理/2022/2022臺北市捷運7-12月(發展程度移動_kriging_v3)5chunk","BoardingTime")
+    
+    TPEmrtdf <- read_fst("E:/brain/解壓縮data/資料處理/2023/2023新北市捷運(發展程度移動_kriging_v3)5.fst",as.data.table = TRUE)
+    TPEmrtdf <- read_fst("E:/brain/解壓縮data/資料處理/2024/2024新北市捷運(發展程度移動_kriging_v3)5.fst",as.data.table = TRUE)
+    setnames(TPEmrtdf,
+             old = c("EntryStationID", "EntryStationName", "EntryTime",
+                     "ExitStationID", "ExitStationName", "ExitTime"),
+             new = c("BoardingStopUID", "BoardingStopName", "BoardingTime",
+                     "DeboardingStopUID", "DeboardingStopName", "DeboardingTime"))
+    TPEmrtdf <- TPEmrtdf[, .SD, .SDcols = keep_cols]
+    hourcheck(TPEmrtdf)
+    TPEmrtdf <- hourtrans(TPEmrtdf)
+    
+    write_fst(TPEmrtdf,"E:/brain/解壓縮data/資料處理/對齊後資料/2022新北捷運最終資料.fst")
+    write_fst(TPEmrtdf,"E:/brain/解壓縮data/資料處理/對齊後資料/2023新北捷運最終資料.fst")
+    write_fst(TPEmrtdf,"E:/brain/解壓縮data/資料處理/對齊後資料/2024新北捷運最終資料.fst")
+  }
+  
   # 臺鐵
   {
     raildf <- read_fst("E:/brain/解壓縮data/資料處理/2022/2022臺鐵(發展程度移動_truncated)5.fst",as.data.table = TRUE)
@@ -109,6 +145,8 @@
     write_fst(raildf,"E:/brain/解壓縮data/資料處理/對齊後資料/2024臺鐵捷運最終資料(北北基桃).fst")
   }
   
+  
+  
   bus2024df       <- bus2024df %>% dplyr::select(all_of(keep_cols))
   NTPmrt2024df    <- NTPmrt2024df %>% dplyr::select(all_of(keep_cols))
   
@@ -116,4 +154,5 @@
   combined_dt_path_v2 <- "E:/brain/解壓縮data/資料處理/2024/2024北北基桃公車和新北捷運(合併_voronoi_v2)1.fst"
   write.fst(combined_dt,combined_dt_path)
   write.fst(combined_dt,combined_dt_path_v2)
+  
 }
